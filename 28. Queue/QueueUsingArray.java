5243df84c52f67f2b0eb91ea9d9dc874ad1c1f8a
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2023-09-25 18:50:12 +0530

    Add files via upload

---
+++ 28. Queue/QueueUsingArray.java
@@ -0,0 +1,62 @@
+public class QueueUsingArray {
+    static class Queue{
+        private int arr[];
+        int size; 
+        int rear;
+
+        Queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+        }
+
+        public boolean isEmpty(){
+            return rear==-1;
+        }
+
+        // enque
+        public void add(int data){
+            if(rear==size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear=rear+1;
+            arr[rear]=data;
+        }
+        // dequeue
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return Integer.MIN_VALUE;
+            }
+
+            int front = arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        // peek
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return Integer.MIN_VALUE;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+        q.remove();
+    }
+}
