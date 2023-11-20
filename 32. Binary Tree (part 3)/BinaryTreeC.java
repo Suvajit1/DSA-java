43040144bc566885e0fa36d818e28e891eeea10a
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2023-11-21 02:32:40 +0530

    Add files via upload

--- 32. Binary Tree (part 3)/BinaryTreeC.java
+++ 32. Binary Tree (part 3)/BinaryTreeC.java
@@ -1,4 +1,8 @@
 // Kth level of a tree
+// lowest common ancestors (Approach 1)
+
+import java.util.ArrayList;
+
 public class BinaryTreeC {
     static class Node {
         int data;
@@ -12,7 +16,8 @@ public class BinaryTreeC {
         }
     }
 
-    public static void klevel(Node root, int level, int k){
+    // Kth level of a tree
+    public static void klevel(Node root, int level, int k){ // O(n)
         if(root==null){
             return ;
         }
@@ -24,6 +29,49 @@ public class BinaryTreeC {
         klevel(root.right, level+1, k);
     }
 
+    // lowest common ancestors (Approach 1)
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+
+        path.add(root);
+
+        if(root.data==n){
+            return true;
+        }
+
+        // boolean foundLeft=getPath(root.left, n, path);
+        // boolean founfRight= getPath(root.right, n, path);
+
+        // if(foundLeft || founfRight){
+        //     return true;
+        // }
+
+        if( getPath(root.left, n, path) || getPath(root.right, n, path) ){
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
+    }
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1= new ArrayList<>();
+        ArrayList<Node> path2= new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i;
+        for(i=0; i<path1.size() && i<path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        // last common ancestor - i-1th index
+        Node lca = path1.get(i-1);
+        return lca;
+    }
     public static void main(String[] args) {
         /*
                     1
@@ -43,6 +91,11 @@ public class BinaryTreeC {
 
         klevel(root, 1, 3);
         System.out.println();
+
+        Node lca1=lca(root, 4, 6);
+        Node lca2=lca(root, 4, 5);
+        System.out.println(lca1.data);        
+        System.out.println(lca2.data);
 
     }
 }
