0c345e97b02842857f53e34a4b5edb60291b8c09
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2023-11-23 03:33:16 +0530

    Add files via upload

---
+++ 33. BST (Part 1)/BST.java
@@ -0,0 +1,79 @@
+// Build a Binary Search Tree 
+// Search for a key in BST 
+
+public class BST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left=insert(root.left, val);
+        }else{
+            root.right= insert(root.right, val);
+        }
+
+        return root;
+    }
+    public static void inOrder(Node root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    public static boolean search(Node root, int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data == key){
+            return true;
+        }
+        
+        if(key>root.data){
+            return search(root.right, key);
+        }else{
+            return search(root.left, key);
+        }
+    }
+    public static void main(String[] args) {
+        int values[]={5,1,3,4,2,7};
+
+        /*
+         *       5
+         *      / \
+         *     1   7
+         *      \
+         *       3
+         *      / \
+         *     2   4
+        */
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root= insert(root, values[i]);
+        }
+
+        inOrder(root);
+        System.out.println();
+
+        System.out.println(search(root, 2));
+        System.out.println(search(root, 9));
+    }
+}
