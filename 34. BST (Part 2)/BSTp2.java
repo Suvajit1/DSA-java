4011a1a9b74dbee48decc89f059aea9ab38bdc81
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2024-01-02 15:18:11 +0530

    Add files via upload

--- 34. BST (Part 2)/BSTp2.java
+++ 34. BST (Part 2)/BSTp2.java
@@ -1,5 +1,16 @@
 // Sorted array to balanced BST 
+// Conver BST to Bananced BST
+/*
+ *               8                      8
+ *              / \                    / \
+ *             6  10                  5   11
+ *            /     \      -->       / \  / \
+ *           5      11              3  6 10 12
+ *          /        \
+ *         3         12
+ */
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 public class BSTp2 {
@@ -67,11 +78,51 @@ public class BSTp2 {
         }
     }
 
+    public static void inOrderArray(Node root, ArrayList<Integer> inOrder){
+        if(root==null){
+            return;
+        }
+        inOrderArray(root.left, inOrder);
+        inOrder.add(root.data);
+        inOrderArray(root.right, inOrder);
+    }
+
+    public static Node balancedBST(ArrayList<Integer> inOrder, int st, int end){
+        if(end<st){
+            return null;
+        }
+
+        int mid= (st+end)/2;
+
+        Node root = new Node(inOrder.get(mid));
+        root.left = balancedBST(inOrder, st, mid-1);
+        root.right = balancedBST(inOrder, mid+1, end);
+
+        return root;
+    }
     public static void main(String[] args) {
         int values[]={3,5,6,8,10,11,12};
         Node root = balancedBST(values, 0, values.length-1);
         preorder(root); 
         System.out.println();
         levelorder(root);
+
+        Node rootu=new Node(8);
+        rootu.left = new Node(6);
+        rootu.left.left = new Node(5);
+        rootu.left.left.left =new Node(3);
+        rootu.right = new Node(10);
+        rootu.right.right = new Node(11);
+        rootu.right.right.right =new Node(12);
+
+        levelorder(rootu);
+
+        ArrayList<Integer> inOrder=new ArrayList<>();
+        inOrderArray(rootu, inOrder);
+        rootu=balancedBST(inOrder, 0, inOrder.size()-1);
+        preorder(rootu);
+        System.out.println();
+        levelorder(rootu);
+
     }
 }
