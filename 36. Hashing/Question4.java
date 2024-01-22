f33ff26b69b316a40695f5a599d847eca2ec382a
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2024-01-22 14:47:18 +0530

    Add files via upload

---
+++ 36. Hashing/Question4.java
@@ -0,0 +1,39 @@
+// union and intersection of two arrays
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Question4 {
+    public static void main(String[] args) {
+        int arr1[]={7,3,9};
+        int arr2[]={6,3,9,2,9,4};
+
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0; i<arr1.length; i++){
+            hs.add(arr1[i]);
+        }
+        for(int i=0; i<arr2.length; i++){
+            hs.add(arr2[i]);
+        }
+
+        // union
+        System.out.println("Union : "+hs);
+        System.out.println("Cardinallity : "+hs.size());
+
+        // intersection
+        hs.clear();
+        for(int i=0; i<arr2.length; i++){
+            hs.add(arr2[i]);
+        }
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0; i<arr1.length; i++){
+            if(hs.contains(arr1[i])){
+                al.add(arr1[i]);
+                hs.remove(arr1[i]);
+            }
+        }
+
+        System.out.println("Intersection : "+al);
+        System.out.println("Cardinallity : "+al.size());
+    }
+}
