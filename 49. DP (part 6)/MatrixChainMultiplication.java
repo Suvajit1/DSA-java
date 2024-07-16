e5f7681cf271ae08c1592aeadd02434b830e67ce
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2024-07-17 02:54:00 +0530

    Add files via upload

--- 49. DP (part 6)/MatrixChainMultiplication.java
+++ 49. DP (part 6)/MatrixChainMultiplication.java
@@ -1,10 +1,12 @@
 // Matrix Chain Multiplication
 
+import java.util.Arrays;
+
 public class MatrixChainMultiplication {
 
     // recursion
-    // Time Comlpexity : O(2^n)
-    public static int mcm(int arr[], int i, int j){
+    // Time Comlpexity : O(K^n)
+    public static int mcmRec(int arr[], int i, int j){
         if(i==j){
             return 0;
         }
@@ -12,8 +14,8 @@ public class MatrixChainMultiplication {
         int ans = Integer.MAX_VALUE;
 
         for(int k=i; k<=j-1; k++){
-            int cost1 = mcm(arr, i, k); // set 1 -> X
-            int cost2 = mcm(arr, k+1, j);   // set 2 -> Y
+            int cost1 = mcmRec(arr, i, k); // set 1 -> X
+            int cost2 = mcmRec(arr, k+1, j);   // set 2 -> Y
 
             int cost3 = arr[i-1] * arr[k] * arr[j]; // cost to find XY
 
@@ -24,13 +26,43 @@ public class MatrixChainMultiplication {
 
         return ans;
     }
-    
+
+    // memorisation
+    // Time Complexity - O(n^2)
+    public static int mcmMem(int[] arr, int i, int j, int dp[][]){
+        if(i==j){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int k=i; k <= j-1; k++){
+            int cost1 = mcmMem(arr, i, k, dp);
+            int cost2 = mcmMem(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+
+            ans = Math.min(ans, (cost1+cost2+cost3));
+        }
+
+        return dp[i][j] = ans;
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
         int n = arr.length; 
 
-        int minCost = mcm(arr, 1, n-1);
+        int minCost = mcmRec(arr, 1, n-1);
 
         System.out.println(minCost);
+
+        int dp[][] = new int[n][n];
+        // initialisation
+        for(int i=0; i<dp.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(mcmMem(arr, 1, n-1, dp));
     }
 }
