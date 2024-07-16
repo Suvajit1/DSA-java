bac9c79a74f9b4cd34fd06130dffb5143db5e039
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2024-07-17 04:26:06 +0530

    Add files via upload

--- 49. DP (part 6)/MatrixChainMultiplication.java
+++ 49. DP (part 6)/MatrixChainMultiplication.java
@@ -50,6 +50,48 @@ public class MatrixChainMultiplication {
         return dp[i][j] = ans;
     }
 
+    // tabulation
+    public static int mcmTab(int[] arr){
+        int n = arr.length;
+
+        // table create
+        int[][] dp = new int[n][n];
+
+        // initialisation
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;   // len = 1
+        }
+
+        // filling (diagonally)
+        for(int len=2; len<=n-1; len++){
+            for(int i=1; i<=n-len; i++){    //row
+                int j = i+len-1;    // col
+                dp[i][j] = Integer.MAX_VALUE;
+
+                // set or group
+                for(int k=i; k<=j-1; k++){
+                    int cost1 = dp[i][k];   // (i...k)
+                    int cost2 = dp[k+1][j]; // (k+1 ...j)
+                    int cost3 = arr[i-1]*arr[k]*arr[j];
+
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+
+        printdp(dp);
+        return dp[1][n-1];  // A1A2 ...A(n-1)
+    }
+
+    public static void printdp(int dp[][]){
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
         int n = arr.length; 
@@ -64,5 +106,7 @@ public class MatrixChainMultiplication {
             Arrays.fill(dp[i], -1);
         }
         System.out.println(mcmMem(arr, 1, n-1, dp));
+
+        System.out.println(mcmTab(arr));
     }
 }
